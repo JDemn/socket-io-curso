7ca0bc4a08a9505d90b83c0fdf5499bafb8d3746
JDemn <dsoftwaremancilla@outlook.es>
2022-05-23 23:43:23 -0500

    server emit a vent message and client listen the event and after the inverse

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const app = express();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+//socket = cliente
+io.on('connection', (socket) => { 
+    console.log(socket.id);
+    socket.emit('mensaje-de-bienvenida', {
+        msg : 'Hola papucho',
+        date : new Date(),
+    })
 
-io.on('connection', () => { console.log('cliente contectado'); });
+    //listening event emited by the client
+    socket.on('sending-client-message',(data)=>{
+        console.log('receiving data from client side',data);
+    })
+});
 
 // Deploy public directory
-app.use(express.static(__dirname + './public'))
+app.use(express.static(__dirname + '/public'))
 
 server.listen(3001, ()=>{
     console.log('server corriendo en puerto 3001');
