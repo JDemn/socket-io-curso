867c5bcc296cb5010609e6c5a6114b7a470c4b63
Deniz <jose.deniz@panzura.com>
2022-05-27 17:12:06 -0500

    server configuration and socket configuration

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-//server express and first socket io connection
+const Server = require('./models/server')
 
-const express = require('express');
-const app = express();
-
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-//socket = cliente
-io.on('connection', (socket) => { 
-    //listening event emited by the client
-    socket.on('sending-client-message',(data)=>{
-        console.log('receiving data from client side',data);
-    })
-
-    //receive message from client
-    socket.on('client-menssage',(message)=>{
-        console.log('mensaje del cliente :', message);
-
-        socket.emit('msg-from-server', message);
-    })
-});
-
-// Deploy public directory
-app.use(express.static(__dirname + '/public'))
-
-server.listen(3001, ()=>{
-    console.log('server corriendo en puerto 3001');
-});
+const server = new Server();
+server.execute();
