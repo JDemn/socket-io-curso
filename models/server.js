589bbbea2148428fea08309698710a573b569c40
Deniz <jose.deniz@panzura.com>
2022-06-06 16:48:43 -0500

    por number default

--- models/server.js
+++ models/server.js
@@ -9,7 +9,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT||3001 ;
 
         //Http server configuration
         this.server = http.createServer(this.app);
